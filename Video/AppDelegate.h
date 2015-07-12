9bbc609b28c0a92dace7e4c940e521ac54e81229
jdong <jdong@jdong.local>
2015-07-13 06:03:54 +0800

    Initial Commit

---
+++ Video/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Video
+//
+//  Created by jdong on 15-7-13.
+//  Copyright (c) 2015年 jdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Video/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Video
+//
+//  Created by jdong on 15-7-13.
+//  Copyright (c) 2015年 jdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
